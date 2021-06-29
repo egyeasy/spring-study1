3820f02f23b654e0c1a9a7c4e9479a1e34b5cab6
egyeasy <dz1120@gmail.com>
2021-06-29 22:08:04 +0900

    implement Posts List

--- src/main/java/com/spring/study1/springboot/service/posts/PostsService.java
+++ src/main/java/com/spring/study1/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.spring.study1.springboot.service.posts;
 
 import com.spring.study1.springboot.domain.posts.Posts;
 import com.spring.study1.springboot.domain.posts.PostsRepository;
+import com.spring.study1.springboot.web.dto.PostsListResponseDto;
 import com.spring.study1.springboot.web.dto.PostsResponseDto;
 import com.spring.study1.springboot.web.dto.PostsSaveRequestsDto;
 import com.spring.study1.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +38,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글일 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
