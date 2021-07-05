c5a3f1694a2d70576c76ffd36b443afb18b8be9d
egyeasy <dz1120@gmail.com>
2021-07-06 00:25:49 +0900

    add annotation based SessionUser resolver

--- src/main/java/com/spring/study1/springboot/web/IndexController.java
+++ src/main/java/com/spring/study1/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.spring.study1.springboot.web;
 
+import com.spring.study1.springboot.config.auth.LoginUser;
 import com.spring.study1.springboot.config.auth.dto.SessionUser;
 import com.spring.study1.springboot.service.posts.PostsService;
 import com.spring.study1.springboot.web.dto.PostsResponseDto;
@@ -18,9 +19,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
